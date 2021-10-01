7304b84a701edba4005fe999e71ba05f573ce10f
Germán <germanguevara2496@gmail.com>
2021-10-01 11:41:57 -0400

    main activity package refactor

--- app/src/main/java/com/toolbox/bestmovies/ui/MainActivity.kt
+++ app/src/main/java/com/toolbox/bestmovies/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.toolbox.bestmovies
+package com.toolbox.bestmovies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.toolbox.bestmovies.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
