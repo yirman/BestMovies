0365551b2ff924c4bf03f6365b67d6f7805b1f5a
Germán <germanguevara2496@gmail.com>
2021-10-02 22:59:24 -0400

    Adding Carousels Fragment and Navigation

--- app/src/main/java/com/toolbox/bestmovies/ui/MainActivity.kt
+++ app/src/main/java/com/toolbox/bestmovies/ui/MainActivity.kt
@@ -2,11 +2,27 @@ package com.toolbox.bestmovies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.toolbox.bestmovies.R
+import com.toolbox.bestmovies.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
