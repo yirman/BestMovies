576d191f258be493e9a3d67d4707e3bfdaf8e197
Germán <germanguevara2496@gmail.com>
2021-10-03 16:37:50 -0400

    movie repo at RepositoryModule

--- app/src/main/java/com/toolbox/bestmovies/di/RepositoryModule.kt
+++ app/src/main/java/com/toolbox/bestmovies/di/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package com.toolbox.bestmovies.di
 
 import com.toolbox.bestmovies.data.local.CarouselDao
+import com.toolbox.bestmovies.data.local.MovieDao
 import com.toolbox.bestmovies.data.remote.CarouselRemoteDataSource
 import com.toolbox.bestmovies.data.repository.CarouselRepository
+import com.toolbox.bestmovies.data.repository.MovieRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,5 +19,10 @@ object RepositoryModule {
     @Provides
     fun provideCarouselRepository(remoteDataSource: CarouselRemoteDataSource, carouselDao: CarouselDao) =
         CarouselRepository(remoteDataSource, carouselDao)
+
+    @ViewModelScoped
+    @Provides
+    fun provideMovieRepository(movieDao: MovieDao) =
+        MovieRepository(movieDao)
 
 }
