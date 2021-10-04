9505e5a9417d7568bfdd810b47154f5011ed5c48
Germán <germanguevara2496@gmail.com>
2021-10-04 00:48:23 -0400

    fixing bottom sheet dialog

--- app/src/main/java/com/toolbox/bestmovies/ui/fragment/MovieInfoFragment.kt
+++ app/src/main/java/com/toolbox/bestmovies/ui/fragment/MovieInfoFragment.kt
@@ -1,24 +1,23 @@
 package com.toolbox.bestmovies.ui.fragment
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.FrameLayout
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.signature.ObjectKey
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.toolbox.bestmovies.R
 import com.toolbox.bestmovies.databinding.FragmentMovieInfoBinding
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
-import kotlin.concurrent.schedule
 
 
 @AndroidEntryPoint
@@ -30,6 +29,18 @@ class MovieInfoFragment : BottomSheetDialogFragment() {
     private val viewModel: MovieViewModel by viewModels()
 
     private var idMovie: Int? = null
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val bottomSheetDialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        bottomSheetDialog.setOnShowListener { dialog ->
+            val dialog1 = dialog as BottomSheetDialog
+            val bottomSheet: FrameLayout = dialog1.findViewById(com.google.android.material.R.id.design_bottom_sheet)!!
+            BottomSheetBehavior.from(bottomSheet).state = BottomSheetBehavior.STATE_EXPANDED
+            BottomSheetBehavior.from(bottomSheet).skipCollapsed = true
+            BottomSheetBehavior.from(bottomSheet).isHideable = true
+        }
+        return bottomSheetDialog
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
